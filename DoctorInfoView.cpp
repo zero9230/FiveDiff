19ea782b146f5d099ec28542ec09fe69f56a9778
XHQS <245906838@qq.com>
2018-07-09 10:24:36 +0800

    modified:   DoctorInfoView.cpp

--- DoctorInfoView.cpp
+++ DoctorInfoView.cpp
@@ -560,12 +560,8 @@ void CDoctorInfoView::OnBnClickedSave()//update one info
 	CString filename;
 	_variant_t var;
 	CString strTemp;
-
-	
-
 	CT2CA tempPassword(doctor_num);
 	std::string strPassword(tempPassword);
-
 	std::regex regPattern("[0-9]*"); // <span style = "white-space:pre">	< / span>// at least 6 charaters of digit or letter
 	BOOL bValid = std::regex_match(strPassword, regPattern);
 	if (!bValid)
@@ -573,10 +569,6 @@ void CDoctorInfoView::OnBnClickedSave()//update one info
 		MessageBox(L"Please enter into the number!");
 		return ;
 	}
-		
-
-
-
 	POSITION pos = m_DoctorInfoList.GetFirstSelectedItemPosition(); // getting current position of list
 	if (pos == NULL)
 		TRACE("No items were selected!\n");
@@ -584,7 +576,7 @@ void CDoctorInfoView::OnBnClickedSave()//update one info
 	{
 		int nItem = m_DoctorInfoList.GetNextSelectedItem(pos);
 		CString doct_id;
-		doct_id.Format(_T("%d"), ThisResult.numofrs[nItem]);
+		doct_id = ThisResult.numofrs[nItem];
 		filename.Format(L"appdata.accdb");
 
 		if (OpenDataBase(filename, m_pDB, m_pRs) == -1)
